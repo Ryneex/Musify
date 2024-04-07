4e06979dcde1c80f95cbe283f059be68a75e420a
MI Sajid <misajidsa@gmail.com>
2024-04-08 00:37:17 +0600

    feat: style verification email

--- src/actions/user/authentication/sendVerificationCode.ts
+++ src/actions/user/authentication/sendVerificationCode.ts
@@ -26,10 +26,11 @@ export default async function sendVerificationCode() {
             },
         })
         await transporter.sendMail({
-            from: 'musajidcoc@gmail.com',
+            from: 'misajidcoc@gmail.com',
             to: res.email,
-            subject: 'Confirm your account',
-            html: `${code}`,
+            subject: 'Musify Verification Code',
+
+            html: getHtml(code),
         })
         const expiresAt = Date.now() + 1000 * 60 * 2
         await User.findOneAndUpdate({ email: res.email }, { verificationCode: { value: code, expiresAt } })
@@ -37,4 +38,23 @@ export default async function sendVerificationCode() {
     } catch (error) {
         return { err: 'Something went wrong' }
     }
+}
+
+function getHtml(code: number) {
+    return `
+<div style="width: 100%; height: 100%; display: flex; align-items: center">
+    <div style="max-width: 600px; margin: 0 auto; padding: 50px; background-color: #fff; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1)">
+        <div style="text-align: center; margin-bottom: 30px">
+            <h2 style="color: #007bff; margin: 0">Musify Email Verification Code</h2>
+        </div>
+        <div style="padding: 20px; background-color: #f9f9f9; border-radius: 8px">
+            <p style="font-size: 20px; font-weight: bold; text-align: center; margin-bottom: 20px; color: #007bff">Your verification code is: <span style="color: #007bff">${code}</span></p>
+            <p style="text-align: center; margin-bottom: 30px">Please use this code to verify your email address.</p>
+        </div>
+        <div style="text-align: center; margin-top: 30px; color: #777">
+            <p>This email was sent automatically. Please do not reply.</p>
+        </div>
+    </div>
+</div>
+`
 }
