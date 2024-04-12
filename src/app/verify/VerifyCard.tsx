ce24da17224647ab0e1e90ecd4d4cac09a6e2a57
MI Sajid <misajidsa@gmail.com>
2024-04-13 00:54:26 +0600

    style: support dark mode

--- src/app/verify/VerifyCard.tsx
+++ src/app/verify/VerifyCard.tsx
@@ -65,7 +65,7 @@ export default function VerifyCard({ email }: any) {
     }, [expiresAt])
 
     return (
-        <div className="flex h-screen items-center justify-center sm:bg-slate-50">
+        <div className="flex h-screen items-center justify-center dark:bg-zinc-950 sm:bg-slate-50">
             <div
                 onClick={() => userStore.toggleTheme()}
                 className="fixed right-2 top-2 aspect-square h-full max-h-9 cursor-pointer select-none rounded-full bg-black/5 text-2xl hover:bg-black/10 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
@@ -78,10 +78,10 @@ export default function VerifyCard({ email }: any) {
             <div className="fixed">
                 <Toaster />
             </div>
-            <div className="w-full max-w-[640px] rounded-xl bg-white p-10 sm:shadow-md">
+            <div className="w-full max-w-[640px] rounded-xl bg-white p-10 dark:bg-zinc-900 sm:shadow-md">
                 {sendNowClicked ? (
                     <div className="flex flex-col items-center gap-4">
-                        <h2 className="text-sm">Enter your verification code</h2>
+                        <h2 className="text-sm dark:text-white">Enter your verification code</h2>
                         <div className="flex flex-col gap-1">
                             <InputOTP
                                 value={code}
@@ -90,23 +90,32 @@ export default function VerifyCard({ email }: any) {
                                 pattern={REGEXP_ONLY_DIGITS}
                             >
                                 <InputOTPGroup>
-                                    <InputOTPSlot className="h-12 w-12 sm:w-16" index={0} />
-                                    <InputOTPSlot className="h-12 w-12 sm:w-16" index={1} />
-                                    <InputOTPSlot className="h-12 w-12 sm:w-16" index={2} />
-                                    <InputOTPSlot className="h-12 w-12 sm:w-16" index={3} />
-                                    <InputOTPSlot className="h-12 w-12 sm:w-16" index={4} />
-                                    <InputOTPSlot className="h-12 w-12 sm:w-16" index={5} />
+                                    <InputOTPSlot className="h-12 w-12 dark:text-white sm:w-16 dark:border-white/20" index={0} />
+                                    <InputOTPSlot className="h-12 w-12 dark:text-white sm:w-16 dark:border-white/20" index={1} />
+                                    <InputOTPSlot className="h-12 w-12 dark:text-white sm:w-16 dark:border-white/20" index={2} />
+                                    <InputOTPSlot className="h-12 w-12 dark:text-white sm:w-16 dark:border-white/20" index={3} />
+                                    <InputOTPSlot className="h-12 w-12 dark:text-white sm:w-16 dark:border-white/20" index={4} />
+                                    <InputOTPSlot className="h-12 w-12 dark:text-white sm:w-16 dark:border-white/20" index={5} />
                                 </InputOTPGroup>
                             </InputOTP>
                             <div className="flex justify-between text-xs">
-                                <span className={`${(timer !== 0 || loading) && 'pointer-events-none opacity-70'}`}>
+                                <span
+                                    className={`${!(timer == 0 || loading) && 'pointer-events-none opacity-70  dark:opacity-80'} dark:text-white`}
+                                >
                                     Didn&apos;t receive it?
-                                    <span onClick={sendCode} className="cursor-pointer text-blue-600">
+                                    <span
+                                        onClick={sendCode}
+                                        className="cursor-pointer text-blue-600 dark:text-blue-400"
+                                    >
                                         {' '}
                                         Resend
                                     </span>
                                 </span>
-                                {timer !== 0 && <span>Resend in {Duration.fromMillis(timer).toFormat('m:ss')}</span>}
+                                {timer !== 0 && (
+                                    <span className="dark:text-white">
+                                        Resend in {Duration.fromMillis(timer).toFormat('m:ss')}
+                                    </span>
+                                )}
                             </div>
                             <Button
                                 disabled={loading || code.length !== 6}
@@ -120,10 +129,13 @@ export default function VerifyCard({ email }: any) {
                     </div>
                 ) : (
                     <div className="flex flex-col items-center gap-5">
-                        <h1 className="text-center text-xl font-bold text-black/80">Verify your Email Address</h1>
-                        <p className="text-center text-sm">
-                            A verification email will be sent to <span className="text-indigo-600">{email}</span>. make
-                            sure to check spam mail. Please click on verify now to proceed
+                        <h1 className="text-center text-xl font-bold text-black/80 dark:text-white/80">
+                            Verify your Email Address
+                        </h1>
+                        <p className="text-center text-sm dark:text-white/80">
+                            A verification email will be sent to{' '}
+                            <span className="text-indigo-600 dark:text-indigo-400">{email}</span>. make sure to check
+                            spam mail. Please click on verify now to proceed
                         </p>
                         <Button
                             disabled={loading}
