2201a320ac9e2b2d9712a987c0f5c052f1774ec9
MI Sajid <misajidsa@gmail.com>
2024-04-26 00:31:19 +0600

    chore: update database name

--- src/db/dbconnect.ts
+++ src/db/dbconnect.ts
@@ -3,7 +3,9 @@ import mongoose from 'mongoose'
 export default async function dbconnect() {
     try {
         if (mongoose.connection?.readyState === 1) return { success: 'MongoDB connected' }
-        await mongoose.connect(process.env.MONGODB_URI as string)
+        await mongoose.connect(process.env.MONGODB_URI as string, {
+            dbName: 'Musify',
+        })
         return { success: 'MongoDB connected' }
     } catch (error) {
         return { error: 'Could not connect to MongoDB' }
