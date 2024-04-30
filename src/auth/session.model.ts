f8ee89c09e3d498333c5cbe2bef29584d0ca3257
MI Sajid <misajidsa@gmail.com>
2024-04-30 19:00:39 +0600

    add session model

--- src/auth/session.model.ts
+++ src/auth/session.model.ts
@@ -1,18 +1,26 @@
-import { type Model, Schema, model, models, SchemaDefinitionProperty } from 'mongoose'
+import User from '@/db/models/user.model'
+import { Schema, model, models, SchemaDefinitionProperty, type Model } from 'mongoose'
 import { v4 } from 'uuid'
 
-export default function getSessionModel(UserModel: Model<any>, name: string) {
-    // prettier-ignore
-    return models[name] || model(name, new Schema({
-        _id: {
-            type: String,
-            default: v4
-        },
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: UserModel,
-            required: true
-        },
-        expiresAt: Date
-    } as SchemaDefinitionProperty));
+interface ISessionModel {
+    _id: string
+    user: string
+    expiresAt: Date
 }
+
+const scema = new Schema({
+    _id: {
+        type: String,
+        default: v4,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: User,
+        required: true,
+    },
+    expiresAt: Date,
+} as SchemaDefinitionProperty)
+
+const Session: Model<ISessionModel> = models.Session || model('Session', scema)
+
+export default Session
