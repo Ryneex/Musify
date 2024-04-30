3a774fd21063597bb86c51c78bfd2b81760442d3
MI Sajid <misajidsa@gmail.com>
2024-04-30 18:55:08 +0600

    add timestamps in playlist scema

--- src/db/models/playlist.model.ts
+++ src/db/models/playlist.model.ts
@@ -1,19 +1,22 @@
 import mongoose from 'mongoose'
 
-const schema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    owner_id: {
-        type: String,
-        required: true,
-    },
-    songs: {
-        type: Array,
-        default: [],
-    },
-} as { [key: string]: mongoose.SchemaDefinitionProperty })
+const schema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        owner_id: {
+            type: String,
+            required: true,
+        },
+        songs: {
+            type: Array,
+            default: [],
+        },
+    } as { [key: string]: mongoose.SchemaDefinitionProperty },
+    { timestamps: true }
+)
 
 const Playlist = mongoose.models.Playlist || mongoose.model('Playlist', schema)
 
