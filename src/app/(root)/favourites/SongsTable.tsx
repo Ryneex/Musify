55f371f261789edf573f97c238d467d53e165ab4
MI Sajid <misajidsa@gmail.com>
2024-05-10 21:27:02 +0600

    fix: table head stays black after switching theme to light

--- src/app/(root)/favourites/SongsTable.tsx
+++ src/app/(root)/favourites/SongsTable.tsx
@@ -15,7 +15,7 @@ export default function SongsTable({ songs }: Props) {
                         <div className="flex h-full w-full items-center justify-center text-slate-500 dark:text-slate-300">You don&apos;t have any favourite Song</div>
                     ) : (
                         <table className="w-full table-fixed">
-                            <thead className="sticky top-0 z-10 h-12 bg-zinc-950 text-sm after:absolute after:bottom-0 after:h-[1px] after:w-full after:bg-white/[0.1]">
+                            <thead className="sticky top-0 z-10 h-12 bg-white text-sm after:absolute after:bottom-0 after:h-[1px] after:w-full after:bg-black/[0.1] dark:bg-zinc-950 dark:after:bg-white/[0.1]">
                                 <tr className="font-medium dark:text-white/80">
                                     <td className="w-9 text-center sm:w-12">#</td>
                                     <td>Title</td>
