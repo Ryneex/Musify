ebc7d5888375df87267801fbdbd1ed95e2bc5329
MI Sajid <misajidsa@gmail.com>
2024-05-14 23:44:51 +0600

    style: make icons smaller

--- src/components/root/TopNavigation.tsx
+++ src/components/root/TopNavigation.tsx
@@ -11,14 +11,14 @@ export default function TopNavigation() {
     return (
         <div className="flex items-center justify-between gap-3 px-2 sm:px-5">
             <Searchbar />
-            <Link className='hidden sm:block' href="https://github.com/Ryneex/Musify" target="_blank">
-                <FaGithub className="text-4xl" />
+            <Link className="hidden sm:block" href="https://github.com/Ryneex/Musify" target="_blank">
+                <FaGithub className="text-3xl" />
             </Link>
-            <div onClick={() => userStore.toggleTheme()} className="hidden aspect-square h-full max-h-9 cursor-pointer select-none items-center justify-center rounded-full bg-black/5 text-2xl hover:bg-black/10 dark:bg-white/20 dark:hover:bg-white/30 sm:flex">
+            <div onClick={() => userStore.toggleTheme()} className="hidden aspect-square size-8 cursor-pointer select-none items-center justify-center rounded-full bg-black/5 text-2xl hover:bg-black/10 dark:bg-white/20 dark:hover:bg-white/30 sm:flex">
                 <MdNightsStay className="hidden dark:block" />
                 <MdOutlineLightMode className="block dark:hidden" />
             </div>
-            <div className="hidden aspect-square h-9 shrink-0 sm:block">
+            <div className="hidden aspect-square h-8 shrink-0 sm:block">
                 <ProfileButton />
             </div>
         </div>
