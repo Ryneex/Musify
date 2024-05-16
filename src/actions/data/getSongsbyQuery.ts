018ab40421c161796c473f2e4f67f240a680ac03
MI Sajid <misajidsa@gmail.com>
2024-05-16 14:27:18 +0600

    feat: add limit prop

--- src/actions/data/getSongsbyQuery.ts
+++ src/actions/data/getSongsbyQuery.ts
@@ -4,12 +4,12 @@ import { Endpoints } from '@/constants/endpoints'
 import FetchJiosaavn from '@/helpers/FetchJiosaavn'
 import { formatSong } from '@/helpers/format.song'
 
-export default async function getSongsbyQuery(query: string) {
+export default async function getSongsbyQuery(query: string, limit = 100) {
     try {
         const { data } = await FetchJiosaavn({
             __call: Endpoints.search.songs,
             q: query,
-            n: 100,
+            n: limit,
         })
         return { songs: data.results.map((e) => formatSong(e)) }
     } catch (error) {
